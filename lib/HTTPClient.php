bc19e8759e8cf1080209de89cdf902bb8fe45086
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-05-14 19:23:17 +0200

    Some improvements to obtaining REST data from js api

--- lib/HTTPClient.php
+++ lib/HTTPClient.php
@@ -9,6 +9,7 @@ class HTTPClient {
 		$this->config = $config;
 	}
 
+	// TODO: Security check on URL to not refer to local file system
 	private function file_get_contents_curl($url, $headers = array(), $redir = true) {
 		$ch = curl_init();
 
@@ -31,6 +32,7 @@ class HTTPClient {
 		return $data;
 	}
 
+	// TODO: Security check on URL to not refer to local file system
 	protected function rawget($url, $headers = array(), $redir = true, $curl = false) {
 
 		$headerstring = '';
@@ -38,41 +40,69 @@ class HTTPClient {
 			$headerstring .= $k . ': ' . $v . "\r\n";
 		}
 		$opts = array(
+			// Documentation on http stream options available here:
+			// * http://www.php.net/manual/en/context.http.php
 			'http'=>array(
 				'method'=>"GET",
 				'header'=> $headerstring,
 				'follow_location' => $redir,
-				'max_redirects' => 1
+				'max_redirects' => ($redir ? 9 : 1)
 			)
 		);
-		error_log("Options: " . var_export($opts, true));
+		error_log("Options: " . json_encode($opts));
 		error_log("Header string: " . $headerstring);
-		error_log("Headers: " . var_export($headers, true));
+		error_log("Headers: " . json_encode($headers));
 		$context = stream_context_create($opts);
 
 		if ($curl) {
 			return $this->file_get_contents_curl($url, $headers, $redir);
 		}
-
-		return file_get_contents($url, false, $context);
-		// return file_get_contents($url);
+		error_log("About to retrieve: " . $url);
+		$rawdata = file_get_contents($url, false, $context);
+		if ($rawdata === false) throw new Exception();
+		return $rawdata;
 	}
 
+
+	/**
+	 * A check if whether a string is valid JSON
+	 * @param  string  $string The string to check
+	 * @return boolean         Returns true if valid JSON
+	 */
+	protected function isJson($string) {
+		json_decode($string);
+		return (json_last_error() == JSON_ERROR_NONE);
+	}
+
+	/**
+	 * Takes data obtained as a string and conditionally perform some parsing of it 
+	 * into other data types, such as XML or JSON.
+	 * @param  array $result  The result object to update
+	 * @param  array $options Options
+	 * @return array          Returns result.
+	 */
 	protected function decode($result, $options) {
 		if (empty($result["data"])) {
 			return $result;
 		}
-		if (!empty($options['xml'])) {
+
+		if (isset($options['xml']) && $options['xml'] == 1) {
 			$result['data'] = json_decode(json_encode(new SimpleXMLElement($result["data"]), true));
-		} else {
+			$result['type'] = 'xml2json';
+		} else if ($this->isJson($result["data"])) {
 			$result['data'] = json_decode($result["data"], true);		
-		}		
+			$result['type'] = 'json';
+		} else {
+			$result['type'] = 'text';
+		}
 		return $result;
 	}
 
 	public function get($url, $options) {
 		$result = array("status" => "ok");
 		$result["data"] = $this->rawget($url);
+
+		// error_log("Got data: " . var_export($result["data"], true)) ;
 
 		$result = $this->decode($result, $options);
 
@@ -85,9 +115,7 @@ class HTTPClient {
 		$subhost = $subconfigobj->getID();
 		$subconfig = $subconfigobj->getConfig();
 
-		
 
-		
 		$config = array("type" => "plain");
 		if ($handler !== 'plain') {
 
