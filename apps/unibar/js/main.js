086cd8bc126606af2aed33b8f2b0fdc501cc4e73
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-06-05 09:25:43 +0200

    Improvements to unibar app, before demo at IoU fagdag. Adding a lot of entries to the menu, and support two levels nested.

--- apps/unibar/js/main.js
+++ apps/unibar/js/main.js
@@ -6,42 +6,232 @@ require(["unibar"], function(unibar) {
     
 
     var menu = [
+    	{
+    		title: "UNINETT",
+    		url: "http://www.uninett.no",
+    		match: "www\.uninett\.no"
+    	},
+    	{
+    		title: "Eureka/Intern", 
+    		url: "http://eureka.uninett.no",
+    		match: "eureka\.uninett\.no",
+    		groups: ["@realm:uninett.no"],
+    		children: [
+    			{
+		    		title: "Eureka",
+		    		url: "http://eureka.uninett.no",
+		    		match: "eureka\.uninett\.no",
+		    		groups: ["@realm:uninett.no"]
+    			},
+    			{
+		    		title: "Kalender",
+		    		url: "http://calendar.uninett.no",
+		    		match: "calendar\.uninett\.no"
+    			},
+    			{
+		    		title: "Planweb",
+		    		url: "https://eureka.uninett.no/planweb",
+		    		match: "eureka.uninett.no.planweb"
+    			},
+    			{
+		    		title: "Agresso",
+		    		url: "http://krone.uninett.no",
+		    		match: "krone.uninett.no"
+    			},
+    			{
+		    		title: "Webmail",
+		    		url: "https://webmail.uninett.no/src/login.php",
+		    		match: "webmail.uninett.no"
+    			}
+    		]
+    	},
+    	{
+			title: "Nett", 
+			url: "https://www.uninett.no/nettleveranse",
+			match: "www.uninett.no.nett",
+			children: [
+				{
+					title: "Driftsportalen", 
+					url: "https://drift.uninett.no",
+					match: "drift.uninett.no"
+				},
+				{
+					title: "Bigbrother", 
+					url: "https://bigbrother.uninett.no",
+					match: "bigbrother.uninett.no",
+					groups: ["@realm:uninett.no"]
+				},
+				{
+					title: "Stager", 
+					url: "http://stager.uninett.no",
+					match: "stager.uninett.no"
+				}
+			]
+		},
+		{
+			title: "Samarbeid", 
+			url: "http://agora.uninett.no",
+			match: "agora\.uninett\.no",
+			children: [
+		    	{
+		    		title: "Agora", 
+		    		url: "http://agora.uninett.no",
+		    		match: "agora\.uninett\.no"
+		    	},
+		    	{
+		    		title: "Webmøter", 
+		    		url: "http://connect.uninett.no",
+		    		match: "connect.uninett.no"
+		    	},
+		    	{
+		    		title: "Foodle", 
+		    		url: "https://foodl.org",
+		    		match: "foodl\.org"
+		    	},
+		    	{
+		    		title: "OpenWiki", 
+		    		url: "https://openwiki.uninett.no",
+		    		match: "openwiki.uninett.no"
+		    	},
+		    	{
+		    		title: "Redmine", 
+		    		url: "https://redmine.uninett.no",
+		    		match: "redmine.uninett.no"
+		    	},
+		    	{
+		    		title: "Cloudstor", 
+		    		url: "https://cloudstor.uninett.no/",
+		    		match: "cloudstor.uninett.no"
+		    		
+		    	}
+			]
+		},
+    	{
+    		title: "Media", 
+    		url: "http://ecampus.no",
+    		match: "ecampus.no",
+    		// groups: ["@orgunit:uninett.no:1e7a0caefeefb920ac3b3501b71f9d2f7943e013"]
+			children: [
+		    	{
+		    		title: "ecampus.no", 
+		    		url: "http://ecampus.no",
+		    		match: "ecampus.no"
+		    	},
+		    	{
+		    		title: "Camtasia Relay", 
+		    		url: "https://relay.ecampus.no/Relay/",
+		    		match: "relay.ecampus.no"
+		    		// groups: ["@orgunit:uninett.no:0a02f6092fa92d03b3b29a792eeafeac7b7f2b11"]
+		    	},
+		    	{
+		    		title: "IP TV", 
+		    		url: "http://forskningsnett.uninett.no/tv/kanaler.html",
+		    		match: "forskningsnett.uninett.no.tv.kanaler.html"
+		    	}
+			]
+    	},
+		{
+			title: "Feide", 
+			url: "http://feide.no/",
+			match: "feide.no",
+			children: [
+		    	{
+		    		title: "feide.no", 
+		    		url: "http://feide.no",
+		    		match: "www.feide.no"
+		    	},
+		    	{
+		    		title: "FeideTools", 
+		    		url: "https://tools.feide.no",
+		    		match: "tools\.feide\.no",
+		    		groups: ["@orgunit:uninett.no:0a02f6092fa92d03b3b29a792eeafeac7b7f2b11"]
+		    	},
+		    	{
+		    		title: "Kundeportal", 
+		    		url: "https://kunde.feide.no",
+		    		match: "tjenester\.uninett\.no",
+		    		groups: ["@orgunit:uninett.no:0a02f6092fa92d03b3b29a792eeafeac7b7f2b11"]
+		    	},
+		    	{
+		    		title: "Innsyn", 
+		    		url: "https://innsyn.feide.no",
+		    		match: "idp.feide.no.simplesaml.module.php.attribViewer"
+		    	},
+		    	{
+		    		title: "DiscoJuice", 
+		    		url: "http://discojuice.org/",
+		    		match: "discojuice\.org"
+		    	},
+		    	{
+		    		title: "SimpleSAMLphp", 
+		    		url: "http://simplesamlphp.org/",
+		    		match: "simplesamlphp.org"
+		    	},
+		    	{
+		    		title: "saml2int", 
+		    		url: "http://saml2int.org/",
+		    		match: "saml2int.org"
+		    	}
+			]
+		},
+	   	{
+    		title: "Tungregning og lagring", 
+    		url: "https://www.uninett.no/regne-og-lagringsressurser",
+    		match: "www.uninett.no.regne-og-lagringsressurser",
+    		// groups: ["@orgunit:uninett.no:1e7a0caefeefb920ac3b3501b71f9d2f7943e013"]
+			children: [
+		    	{
+		    		title: "Sigma", 
+		    		url: "https://www.uninett.no/sigma",
+		    		match: "www.uninett.no.sigma"
+		    	},
+		    	{
+		    		title: "Notur", 
+		    		url: "http://www.notur.no/",
+		    		match: "notur.no"
+		    	},
+		    	{
+		    		title: "Norgrid", 
+		    		url: "http://www.norgrid.no",
+		    		match: "norgrid.no"
+		    	},
+		    	{
+		    		title: "Norstore", 
+		    		url: "http://www.norstore.no/",
+		    		match: "norstore.no"
+		    	}
+			]
+    	},
+	   	{
+    		title: "Administrativt", 
+    		url: "https://fasportalen.uninett.no/portal/",
+    		match: "fasportalen.uninett.no",
+			children: [
+		    	{
+		    		title: "Fasportalen", 
+		    		url: "https://fasportalen.uninett.no/portal/",
+		    		match: "fasportalen.uninett.no"
+		    	},
+		    	{
+		    		title: "Basware", 
+		    		url: "https://uninett.basware-saas.com/invoice/Global_Login.asp",
+		    		match: "uninett.basware-saas.com"
+		    	},
+		    	{
+		    		title: "Visma", 
+		    		url: "https://www.eu-supply.com/login.asp?B=KGV&target=&timeout=",
+		    		match: "www.eu-supply.com"
+		    	},
+		    	{
+		    		title: "Contempus", 
+		    		url: "http://contempus.no",
+		    		match: "contempus.no"
+		    	}
+			]
+    	},
 
-    	{
-    		title: "Eureka", 
-    		url: "http://agora.uninett.no",
-    		groups: ["@realm:uninett.no"]
 
-    	},
-    	{
-    		title: "Agora", 
-    		url: "http://agora.uninett.no"
-    	},
-    	{
-    		title: "Foodle", 
-    		url: "https://beta.foodl.org",
-    		match: "foodl\.org"
-    	},
-    	{
-    		title: "Kundeportal", 
-    		url: "https://kunde.feide.no",
-    		groups: ["@orgunit:uninett.no:0a02f6092fa92d03b3b29a792eeafeac7b7f2b11"]
-    	},
-    	{
-    		title: "FeideTools", 
-    		url: "https://tools.feide.no",
-    		groups: ["@orgunit:uninett.no:0a02f6092fa92d03b3b29a792eeafeac7b7f2b11"]
-    	},
-    	{
-    		title: "eCampus", 
-    		url: "https://kunde.feide.no",
-    		groups: ["@orgunit:uninett.no:1e7a0caefeefb920ac3b3501b71f9d2f7943e013"]
-    	},
-    	{
-    		title: "DiscoJuice docs", 
-    		url: "http://bridge.uninett.no/bar.html",
-    		match: "bridge\.uninett\.no"
-    	}
+
     ];
     
     console.log("Loading complete");
@@ -69,18 +259,18 @@ require(["unibar"], function(unibar) {
 		menuSetup(menu);
 	}
 
-	function active(item) {
+	function isActive(item) {
 
 		if (!item.match) return false;
 		var r = new RegExp(item.match, "gi");
 		var cu = decodeURIComponent(window.location.hash.substr(1));
-		console.log("comparing against", cu);
-		return (r.exec(window.location.hash));
+		// console.log("comparing against", cu, item.match, (r.exec(window.location.hash)));
+		return !!(r.exec(cu));
 	}
 
 	function include(includegroups, usergroups) {
 
-		console.log("Check if user member of these groups ", usergroups, " should access with this acl ", includegroups);
+		// console.log("Check if user member of these groups ", usergroups, " should access with this acl ", includegroups);
 
 		var k, i;
 		for(k in usergroups) {
@@ -91,24 +281,77 @@ require(["unibar"], function(unibar) {
 		return false;
 	} 
 
-	function menuSetup(menu, user) {
-		$("div#menu").empty();
-		$.each(menu, function(i, item) {
+	function menuTraverse(menu, user) {
+		var
+			active = false,
+			activecount = 0,
+			cmenu;
+
+		cmenu = menu.reverse();
+		$.each(cmenu, function(i, item) {
 
 			if(item.groups && !user) return;
 			if (item.groups && !include(item.groups, user.groups)) return;
 
-			console.log("entry", item);
-			var mi = $('<div class="menuitem"><a target="_parent" href="' + item.url + '">' + item.title + '</a></div>');
+			cmenu[i].show = true;
 
-			if (active(item)) {
-				$(mi).addClass("menuitem_active");
+			if (isActive(item) && activecount++ < 1) {
+
+				cmenu[i].active = true;
+				active = true;
 			}
 
-			$("div#menu").append(mi);
-			console.log(mi);
-			console.log($("div#menu"));
+			if (item.hasOwnProperty("children")) {
+				if (menuTraverse(cmenu[i].children, user)) {
+					active = true;
+					cmenu[i].active = true;
+					cmenu[i].show = true;
+				}
+			}
+
 		});
+		menu.reverse();
+
+		// console.log("Processing row ", active, menu);
+
+		return active;
+	}
+
+	function menuRowDisplay(menu, user) {
+		var nextlevel = null;
+		menurow = $('<div class="menurow"></div>');
+
+		$.each(menu, function(i, item) {
+
+			if(!item.show) return;
+
+			var mi = $('<div class="menuitem"><a target="_parent" href="' + item.url + '">' + item.title + '</a></div>');
+
+			if (item.active) {
+				$(mi).addClass("menuitem_active");
+				if (item.hasOwnProperty("children")) {
+					nextlevel = item.children;
+				}
+			}
+			menurow.append(mi);
+
+		});
+		$("div#menu").append(menurow);
+		if (nextlevel) {
+			// console.log("Preparing next level", nextlevel);
+			menuRowDisplay(nextlevel, user);
+		}
+	}
+
+	function menuSetup(menu, user, empty) {
+	
+		$("div#menu").empty();	
+
+		menuTraverse(menu, user);
+		console.log(menu); // return;
+
+		menuRowDisplay(menu, user);
+		
 	}
 
 
---
+++ apps/unibar/js/unibar-embed.js
@@ -0,0 +1,6 @@
+var embed = '<iframe src="http://unibar.app.bridge.uninett.no/#' + encodeURIComponent(window.location.href) + '" ' +
+	'style="overflow: none; border: none; padding: 0px; margin: 0px; width: 100%; height: 40px; border-bottom: 2px solid #222; z-index: 10" ></iframe>';
+document.write(embed);
+$(document).ready(function() {
+	$("img#ulogo").hide();	
+});
