d444639b621b07d7622b97cee65cbf68a840dea7
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-06-19 20:40:32 +0200

    adding script to update in a cron

---
+++ bin/update.php
@@ -0,0 +1,100 @@
+#!/usr/bin/env php
+<?php
+
+
+$uwapBaseDir = dirname(dirname(__FILE__));
+$autoload = $uwapBaseDir . '/lib/autoload.php';
+
+# error_log("Checking basedir: " . $autoload); exit;
+
+/* Add library autoloader. */
+require_once($autoload);
+
+$program = array_shift($argv);
+if (count($argv) > 0) {
+	echo "Wrong number of parameters. Run:   " . $program . " .... TBD\n"; 
+	exit(2);
+}
+
+
+function h($p) {
+	$hash = base64_encode(sha1($p, true));
+	// return crypt(crypt($p, base64_encode($p)));
+	// return crypt($p,rand(10000, 99999));
+	return '{SHA}'.$hash;
+}
+
+
+$store = new UWAPStore();
+$lookup = $store->queryList('davcredentials', array(), array('username', 'password'));
+
+$pwdfile = '';
+
+foreach($lookup AS $up) {
+	$line = $up['username'] . ':' . h($up['password']) . "\n";
+	$pwdfile .= $line;
+}
+
+// echo $pwdfile;
+// echo "store to " . $uwapBaseDir . '/passwords'; exit;
+file_put_contents($uwapBaseDir . '/passwords', $pwdfile);
+
+// print_r($lookup);
+
+
+
+
+
+$ac = new Config(false);
+$listing = $ac->getAllApps();
+
+
+foreach($listing["app"] AS $app) {
+
+
+	$current = new Config($app["id"]);
+	$config = $current->getConfig();
+
+	if (empty($config['uwap-userid'])) {
+		echo "Skipping [" . $config["name"] . "] without owner.\n";
+		print_r($config);
+		continue;
+	}
+
+	$p = $current->getAppPath();
+	$credentials = $current->getDavCredentials();
+
+
+	echo "\n\n------------------------------------\n";
+	echo "Processing [" . $app["name"] . "]\n";
+
+	
+
+	if (is_dir($p)) {
+		echo " Dir " . $p . " exists.\n";
+	} else {
+		mkdir($p);
+		echo " Dir " . $p . " does not exists.\n";
+	}
+
+	$hta = $p . '.htaccess';
+
+	if (file_exists($hta )) {
+		echo " .htaccess file exists\n";
+	} else {
+		echo " .htaccess file does not exists\n";
+		file_put_contents($hta, "Require user " . $credentials["username"] . "\n");
+	}
+
+	// print_r($credentials);
+	// echo json_encode($app) . "\n";
+
+
+
+}
+
+
+
+
+
+
