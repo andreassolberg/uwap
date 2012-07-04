5bc18056407a54919b19ff65ac0391caafd5f3d2
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-07-04 13:24:17 +0200

    Adding two templates

---
+++ bootstrap/boilerplate/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	/**
+	 * Welcome to UWAP. Replace this section with your own main script.
+	 */
+
+	 $("p.helloworld")
+	 	.empty()
+	 	.append('Hello world!');
+
+});
