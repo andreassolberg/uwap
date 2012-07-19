acdb9b5e7d7c9217a063f6faeeedca8cdbe6f49d
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-07-19 11:28:41 +0200

    update adressa on solberg dashboard app

--- apps/solberg/js/main.js
+++ apps/solberg/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	
+	function cdata(s) {
+		var matches=[];
+		s.replace(/\<\!\[CDATA\[(.+?\]{0}\>{0})\]\]\>/g, function(a,b){matches.push(b)});
+		return matches.join(' ');
+	}
+
+
 	var dataurl = 'http://location.app.bridge.uninett.no/js/locations.js';
 
 	// console.log("Loaded data", locationconfig);
@@ -8,13 +15,25 @@ $(document).ready(function() {
 		console.log("Loaded all set go.")
 		console.log(locationconfig); 
 
+
+		var adressa = new Adressa($("div#adressa"));
 		var tracker = new Tracker(locationconfig, $("div#locations"));
 		var scalendar = new SCalendar($("div#calendar"));
-		// var adressa = new Adressa($("div#adressa"));
 		var yr = new Yr($("div#yr"));
 		var msg = new SMsg($("div#messages"));
 		var buss = new Buss($("div#buss"));
 		var gaver = new Gaver($("div#gaver"));
+
+		// UWAP.data.get('http://www.adressa.no/nyheter/',  null, function(data) {
+		// 	console.log("Got addreasa");
+		// 	// console.log(data);
+
+		// 	var obj = $(data);
+		// 	var content = obj.find("div#content div.polarisStories div.article");
+		// 	console.log(content);
+		// 	$("div#adressa").append(content);
+
+		// });
 		
 		// var todo = new Todo($("div#todo"));
 		
--- apps/solberg/js/plugins/Adressa.js
+++ apps/solberg/js/plugins/Adressa.js
@@ -2,14 +2,25 @@ define(function() {
 
 	var Adressa = function(container) {
 		this.container = container;
-		this.url = 'http://m.adressa.no';
-		$(this.container).append('<iframe class="adressa" src="' + this.url + '"></iframe>');
+		this.url = 'http://www.adressa.no/nyheter/';
+	
 		
 		setInterval($.proxy(this.refresh, this), 3*60*1000); // 3 minutes
+		this.refresh();
 	};
 
 	Adressa.prototype.refresh = function() {
-		$(this.container).find("iframe.adressa").attr("src", this.url + "?rand=" + Math.random());
+		var that = this;
+
+		UWAP.data.get(this.url,  null, function(data) {
+			var obj = $(data);
+			var content = obj.find("div#content div.polarisStories div.article");
+			console.log(content);
+			$(that.container).empty().append(content);
+
+		});
+
+
 	}
 
 	return Adressa;
