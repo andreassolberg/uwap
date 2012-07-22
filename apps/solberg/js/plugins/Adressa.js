4ee1028b856e75a4e9895a23ff1cbe1aa85dc6a3
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-07-22 21:14:32 +0200

    adding adressa front page

--- apps/solberg/js/plugins/Adressa.js
+++ apps/solberg/js/plugins/Adressa.js
@@ -6,8 +6,18 @@ define(function() {
 	
 		
 		setInterval($.proxy(this.refresh, this), 3*60*1000); // 3 minutes
+		setInterval($.proxy(this.refreshcover, this), 60*60*1000); // 3 minutes
 		this.refresh();
+		this.refreshcover();
 	};
+
+	Adressa.prototype.refreshcover = function() {
+
+		var html = '<img src="http://static.buyandread.com/thumbnail/adresseavisen/adresseavisen.jpg" style="border-top: 1px solid #333; border-left: 1px solid #333; position: fixed; bottom: 0px; right: 0px" />';
+		$("div#adressacover").empty().append(html)
+		
+		
+	}
 
 	Adressa.prototype.refresh = function() {
 		var that = this;
