86df998b679c73216caaaf118686f9d42dd58aae
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-11-15 20:53:42 +0100

    Updating twitter bootstrap to use built in hosted version instead.

--- bootstrap/twitter/js/main.js
+++ bootstrap/twitter/js/main.js
@@ -1,11 +1,37 @@
-$(document).ready(function() {
-	
-	/**
-	 * Welcome to UWAP. Replace this section with your own main script.
-	 */
+define(function(require, exports, module) {
 
-	 $("p.helloworld")
-	 	.empty()
-	 	.append('Hello world!');
+	var 
+		$ = require('jquery'),
+		UWAP = require('uwap-core/js/core'),
+
+		moment = require('uwap-core/js/moment'),
+		prettydate = require('uwap-core/js/pretty')
+		;
+
+	
+	require('uwap-core/js/jquery.tmpl');
+
+	require('uwap-core/bootstrap/js/bootstrap');
+	require('uwap-core/bootstrap/js/bootstrap-collapse');
+	require('uwap-core/bootstrap/js/bootstrap-button');
+	require('uwap-core/bootstrap/js/bootstrap-dropdown');
+
+	// require('uwap-core/bootstrap/js/bootstrap-modal');
+    // require('uwap-core/bootstrap/js/bootstrap-tooltip');
+	// require('uwap-core/bootstrap/js/bootstrap-transition');
+	// require('uwap-core/bootstrap/js/bootstrap-alert');
+	// require('uwap-core/bootstrap/js/bootstrap-scrollspy');
+	// require('uwap-core/bootstrap/js/bootstrap-tab');
+	// require('uwap-core/bootstrap/js/bootstrap-popover');
+	// require('uwap-core/bootstrap/js/bootstrap-carousel');
+	// require('uwap-core/bootstrap/js/bootstrap-typeahead');
+
+	$("document").ready(function() {
+		
+		// Hello world...
+
+	});
+
+
 
 });
