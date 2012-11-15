2d7047025a4ec3d53b1c67bc5d9f2441f4f01d6a
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-11-15 20:34:39 +0100

    Add more logging on http client

--- lib/HTTPClientToken.php
+++ lib/HTTPClientToken.php
@@ -7,6 +7,8 @@ class HTTPClientToken extends HTTPClientUserAuth {
 	public function get($url, $options) {
 		$result = array("status" => "ok");
 
+		error_log('HTTPClientToken BEGIN');
+
 		if (empty($this->config["token_hdr"])) throw new Exception("missing handler configuration [token_hdr]");
 		if (empty($this->config["token_val"])) throw new Exception("missing handler configuration [token_val]");
 
@@ -27,7 +29,7 @@ class HTTPClientToken extends HTTPClientUserAuth {
 		if (isset($options['curl']) && $options['curl'] === true) {
 			$curl = true;
 		}
-
+		
 		if (isset($this->config['userinfo'])) {
 			error_log('Config: ' . json_encode($this->config['userinfo']));
 			if (isset($this->config['userinfo']['userid']) && $this->config['userinfo']['userid'] === true) {
@@ -35,14 +37,16 @@ class HTTPClientToken extends HTTPClientUserAuth {
 			}
 		}
 
-		error_log('Handler: ' . json_encode($options['handler']));
-		error_log('URL: ' . json_encode($options['url']));
-		error_log('Headers: ' . json_encode($headers));
-		error_log('Redirect?: ' . json_encode($redir));
-		error_log('Options: ' . json_encode($options));
+		error_log('HTTPClientToken Handler: ' . json_encode($options['handler']));
+		error_log('HTTPClientToken URL: ' . json_encode($options['url']));
+		error_log('HTTPClientToken Headers: ' . json_encode($headers));
+		error_log('HTTPClientToken Redirect?: ' . json_encode($redir));
+		error_log('HTTPClientToken Options: ' . json_encode($options));
 
 		// ($url, $headers = array(), $redir = true, $curl = false, $options = array()) {
 		$result["data"] = $this->rawget($url, $headers, $redir, $curl, $options);
+
+		error_log('HTTPClientToken data: ' . $result["data"]);
 
 		$result = $this->decode($result, $options);
 		return $result;
