1d7630eb1255db9349987e9c4383e1359d290337
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2012-12-10 11:19:47 +0100

    minor update to test app

--- apps/test/js/main.js
+++ apps/test/js/main.js
@@ -42,6 +42,14 @@ define(function(require, exports, module) {
 
 		$("document").ready(function() {
 
+
+			var calurl = "http://app.solweb.no/solberg/index.php";
+			UWAP.data.get(calurl, {handler: "solberg"}, function(data) {
+				console.log("DATA RECEIVED");
+				console.log(data);
+			});
+
+
 			$("input#smt").on("click", function() {
 				UWAP.auth.require(function(data) {
 					$("div#out").append("<h2>You are logged in (required check) as - <i>" + data.name + "</i></h2>");
@@ -107,28 +115,28 @@ define(function(require, exports, module) {
 				// 	$("div#out").append('<pre>info: ' + JSON.stringify(data, null, 4) + '</pre>');
 				// });
 
-				UWAP.groups.listMyGroups(function(data) {
-					$.each(data, function(i, item) {
-						var e = '';
-						e += '<p><span style="font-size: 16pt">' + item.title + '</span> - <span>' + item.description + '</span></p>';
-						e += '<pre>' + JSON.stringify(item) + '</pre>';
-						$("div#out").append(e);
-					});
-				})
+				// UWAP.groups.listMyGroups(function(data) {
+				// 	$.each(data, function(i, item) {
+				// 		var e = '';
+				// 		e += '<p><span style="font-size: 16pt">' + item.title + '</span> - <span>' + item.description + '</span></p>';
+				// 		e += '<pre>' + JSON.stringify(item) + '</pre>';
+				// 		$("div#out").append(e);
+				// 	});
+				// })
 
 
-				UWAP.people.query('uninett.no', 'andreas', function(data) {
-					// $("div#out").append('<pre>Search result: ' + JSON.stringify(data, null, 4) + '</pre>');
-					$.each(data, function(i, item) {
-						var e = '';
-						e += '<h3>' + item.name + '</h3>';
-						e += '<p>' + item.o + ' ' + item.mail + '</p>';
-						if (item.jpegphoto) {
-							e += '<img style="max-height: 64px; border: 1px solid #ccc" src="data:image/jpeg;base64,' + item.jpegphoto + '" />';
-						}
-						$("div#out").append(e);
-					});
-				});
+				// UWAP.people.query('uninett.no', 'andreas', function(data) {
+				// 	// $("div#out").append('<pre>Search result: ' + JSON.stringify(data, null, 4) + '</pre>');
+				// 	$.each(data, function(i, item) {
+				// 		var e = '';
+				// 		e += '<h3>' + item.name + '</h3>';
+				// 		e += '<p>' + item.o + ' ' + item.mail + '</p>';
+				// 		if (item.jpegphoto) {
+				// 			e += '<img style="max-height: 64px; border: 1px solid #ccc" src="data:image/jpeg;base64,' + item.jpegphoto + '" />';
+				// 		}
+				// 		$("div#out").append(e);
+				// 	});
+				// });
 
 
 				// UWAP.groups2.removeMember('f5be0115-ffd0-4dda-a1fd-ee78b9a62d29', u.userid, function(data) {
