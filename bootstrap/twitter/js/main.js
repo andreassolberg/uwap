cceec95da16ab37ae94d17d6da731c0a449962d6
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2013-02-09 17:32:11 +0100

    Updated twitter bootstrap boiler template

--- bootstrap/twitter/js/main.js
+++ bootstrap/twitter/js/main.js
@@ -2,36 +2,22 @@ define(function(require, exports, module) {
 
 	var 
 		$ = require('jquery'),
-		UWAP = require('uwap-core/js/core'),
-
-		moment = require('uwap-core/js/moment'),
-		prettydate = require('uwap-core/js/pretty')
+		UWAP = require('uwap-core/js/core')
 		;
 
-	
-	require('uwap-core/js/jquery.tmpl');
 
 	require('uwap-core/bootstrap/js/bootstrap');
-	require('uwap-core/bootstrap/js/bootstrap-collapse');
-	require('uwap-core/bootstrap/js/bootstrap-button');
-	require('uwap-core/bootstrap/js/bootstrap-dropdown');
+	// require('uwap-core/bootstrap/js/bootstrap-collapse');
+	// require('uwap-core/bootstrap/js/bootstrap-button');
+	// require('uwap-core/bootstrap/js/bootstrap-dropdown');
 
-	// require('uwap-core/bootstrap/js/bootstrap-modal');
-    // require('uwap-core/bootstrap/js/bootstrap-tooltip');
-	// require('uwap-core/bootstrap/js/bootstrap-transition');
-	// require('uwap-core/bootstrap/js/bootstrap-alert');
-	// require('uwap-core/bootstrap/js/bootstrap-scrollspy');
-	// require('uwap-core/bootstrap/js/bootstrap-tab');
-	// require('uwap-core/bootstrap/js/bootstrap-popover');
-	// require('uwap-core/bootstrap/js/bootstrap-carousel');
-	// require('uwap-core/bootstrap/js/bootstrap-typeahead');
-
-	$("document").ready(function() {
-		
-		// Hello world...
+	$(document).ready(function() {
+		$(".loader-hideOnLoad").hide();
+		$("#main").append("Hello world!");
 
 	});
 
 
 
 });
+
