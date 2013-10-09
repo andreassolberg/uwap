8e22a9917de825bf9526032da585a32df0bcce14
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2013-10-09 09:07:48 +0200

    Extract external groups from node.js script

--- lib/GroupManager.php
+++ lib/GroupManager.php
@@ -188,6 +188,14 @@ class GroupManager {
 
 
 
+
+
+
+
+
+
+
+
 		// $agora = new GroupFetcherAgora($this->userid);
 		// $agroups = $agora->getGroups();
 
@@ -259,6 +267,33 @@ class GroupManager {
 
 
 
+
+		$extGroups = $this->getExtGroups();
+
+
+		// echo '<pre>external groups:'; print_r($extGroups); exit;
+
+		if ($extGroups !== null) {
+
+
+			foreach($extGroups AS $key => $eg) {
+				$ne = array();
+
+				$ne['id'] = $key;
+				$ne['title'] = $eg['title'];
+
+				$ne['owner'] = ($eg['role'] === 'owner');
+				$ne['admin'] = ($eg['role'] === 'admin');
+				$ne['member'] = true;
+				$ne['listmembers'] = false;
+
+				$result[] = $ne;
+			}
+
+		}
+
+
+
 		// $agora = new GroupFetcherAgora($this->userid);
 		// $agroups = $agora->getGroups();
 		// $result = array_merge($result, $agroups);
@@ -284,6 +319,71 @@ class GroupManager {
 
 		return $result;
 	}
+
+
+
+	public function getExtGroups() {
+
+		$input = array(
+			'userid' => $this->userid,
+		);
+		if (preg_match('/(.*?)@(.*?)$/', $this->userid, $matches)) {
+			$input['realm'] = $matches[2];
+		}
+		$input['idp'] = 'https://idp.feide.no';
+
+		$inputstr = json_encode($input);
+
+		$cmd = '/root/nvm/v0.10.19/bin/node /root/groupengine/getbyuser.js';
+		$descriptorspec = array(
+			0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+			1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+			2 => array("file", "/tmp/uwap-group-output.txt", "a") // stderr is a file to write to
+		);
+
+		$cwd = '/tmp';
+		$env = array('some_option' => 'aeiou');
+
+		$process = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
+
+		// echo "return value"; print_r($cmd);
+
+		// echo "About to start"; print_r($input);
+
+		if (is_resource($process)) {
+			// $pipes now looks like this:
+			// 0 => writeable handle connected to child stdin
+			// 1 => readable handle connected to child stdout
+			// Any error output will be appended to /tmp/error-output.txt
+
+			fwrite($pipes[0], $inputstr);
+			fclose($pipes[0]);
+
+			$result = stream_get_contents($pipes[1]);
+			fclose($pipes[1]);
+
+			// It is important that you close any pipes before calling
+			// proc_close in order to avoid a deadlock
+			$return_value = proc_close($process);
+
+
+
+			if ($return_value === 0) {
+				$parsedgroups = json_decode($result, true);
+
+
+				return $parsedgroups['groups'];
+
+				// echo '<pre>';
+				// print_r($parsedgroups);
+				// exit;
+			}
+
+			// echo "command returned $return_value\n";
+		}
+		return null;
+	}
+
 
 
 	public function addGroup($group) {
