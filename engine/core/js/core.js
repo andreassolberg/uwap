7dacb7a5331ba7c91779f9798d9898526d4c7293
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2014-01-13 11:18:36 +0100

    Adding convenient hash javascript function. Needed for generating appids in autoconfigure widget

--- engine/core/js/core.js
+++ engine/core/js/core.js
@@ -30,6 +30,18 @@ define(function(require) {
 		return (token !== null);
 	}
 
+	UWAP.utils.hash = function(str){
+		var hash = 0;
+		if (str.length == 0) return hash;
+		for (i = 0; i < str.length; i++) {
+			char = str.charCodeAt(i);
+			hash = ((hash<<5)-hash)+char;
+			hash = hash & hash; // Convert to 32bit integer
+		}
+		return Math.abs(hash).toString(36);
+		// return hash.toString(36);
+	}
+
 	/*
 	 * Returns a random string
 	 */
